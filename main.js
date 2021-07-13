a5b83f3d4956e0600e06f28bbf7553c1af9f9e9f
AAYUSH116 <82816625+AAYUSH116@users.noreply.github.com>
2021-07-13 17:36:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+objects=[];
 video="";
 status="";
 
@@ -11,7 +12,29 @@ function setup(){
 }
 function draw(){
     image(video,0,0,480,380);
+    if(status!=""){
+        object.detect(video,gotResults);
+        for(i=0; i<objects.length;i++){
+            document.getElementById("status").innerHTML="Status:object detected";
+            document.getElementById("number_of_objects").innerHTML="number of objects detected:"+objects.length;
+            fill('#FF0000');
+            percent=floor(objects[i].confidence*100);
+            text(objects[i].label+" "+percent+"%",objects[i].x,objects[i].y);
+            noFill()
+            stroke('#FF0000');
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+        }
+    }
 }
+function gotResults(error,results){
+if(error){
+    console.log(error);
+}
+console.log(results);
+objects=results;
+
+}
+
 function start(){
     objectDetector=ml5.objectDetector('cocossd',modelLoaded);
     document.getElementById("status").innerHTML='Status:object detecting';
